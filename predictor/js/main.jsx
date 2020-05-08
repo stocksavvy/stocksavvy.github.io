524bdaf1e7dd29fcb0201795501dffd79c09f737
Sahil Dogra <dograsahil00@gmail.com>
2020-05-07 21:20:21 -0400

    Minor changes to HTML

--- predictor/js/main.jsx
+++ predictor/js/main.jsx
@@ -3,6 +3,6 @@ import ReactDOM from 'react-dom';
 
 // This method is only called once
 ReactDOM.render(
-    <h1>Hello, world!</h1>,
+    <h1>Enter the stock symbol (i.e. AAPL, GOOGL, etc.) to view future predictions! </h1>,
     document.getElementById('reactEntry')
   );
